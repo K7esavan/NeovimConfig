bb406c6c73a7d8ffb0a90c7d4be0ad653b2f781f
K7esavan <kesavan.m@gofrugal.com>
2022-06-04 22:37:31 +0530

    more options for bufferline

--- lua/setup.lua
+++ lua/setup.lua
@@ -1,14 +1,27 @@
-
-require("bufferline").setup {}
+vim.opt.termguicolors = true
+require("bufferline").setup {
+    options = {
+        mode = "buffers",
+        numbers = "none", 
+        indicator_icon = '▎',
+        buffer_close_icon = '',
+        modified_icon = '●',
+        close_icon = '',
+        left_trunc_marker = '',
+        right_trunc_marker = '',
+        tab_size = 18,
+        color_icons = true,
+    }
+}
 
 
 require('nvim-autopairs').setup{}
 
 
 require('telescope').setup({
-  defaults = {
-    layout_config = {
-      vertical = { width = 0.5 }
+    defaults = {
+        layout_config = {
+            vertical = { width = 0.5 }
+        },
     },
-  },
 })
